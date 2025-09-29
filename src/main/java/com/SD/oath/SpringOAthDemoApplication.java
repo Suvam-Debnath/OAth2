73ce4176c17d4a5f5ba0c7c867cae0921857f2b3
Suvam Debnath <debnathsuvam322@gmail.com>
2025-09-29 08:08:45 +0530

    initial commit

---
+++ src/main/java/com/SD/oath/SpringOAthDemoApplication.java
@@ -0,0 +1,13 @@
+package com.SD.oath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOAthDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOAthDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SD/oath/SpringOAthDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SD.oath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOAthDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
